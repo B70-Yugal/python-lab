d2f36b60d74bbb6034b9bc299de3ef8fa9ebcce5
B70-Yugal <80021286+B70-Yugal@users.noreply.github.com>
2021-03-29 22:24:07 +0530

    diamond

---
+++ pattern7.py
@@ -0,0 +1,15 @@
+'''
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+'''
+n=eval(input('enter rows'))
+for i in range(1,n+1):
+    print(' '*(n-i)+'*'*i+(i-1)*'*')
+for i in range(1,n):
+    print((i) * ' ' + (n - i) * '*' + (n - 1 - i) * '*')
+
